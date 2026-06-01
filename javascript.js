94f51ee9436c154c721fde705b6d9bb4dfc38b3e
Vic <123456wangtom@gmail.com>
2026-06-01 16:30:07 +1200

    Can only calculate when user input two number at a one time

--- javascript.js
+++ javascript.js
@@ -17,23 +17,23 @@ function divide(num1, num2) {
     return num1 / num2
 }
 
-const num1 = '';
-const num2 = '';
-const operator = '';
+let num1 = '';
+let num2 = '';
+let operator = '';
 
 function operate(num1, operator, num2) {
     let result = 'no result'
 
-    if (operator === "add")
+    if (operator === "+")
         result = add(num1, num2)
 
-    if (operator === "subtract")
+    if (operator === "-")
         result = subtract(num1, num2)
 
-    if (operator === "multiply")
+    if (operator === "*")
         result = multiply(num1, num2)
 
-    if (operator === "divide")
+    if (operator === "/")
         result = divide(num1, num2)
 
     return result
@@ -50,6 +50,7 @@ buttons.forEach((button) => {
     button.addEventListener("click", () => {
         displayOnScreen.textContent += button.textContent
 
+
         if (button.textContent == '+' || button.textContent == '-' || button.textContent == '*' || button.textContent == '/') {
             operator = button.textContent
         }
@@ -57,11 +58,20 @@ buttons.forEach((button) => {
 
         if (button.textContent == '=') {
 
-            const messageArray = displayOnScreen.textContent.split(operator)
-            num1 = +messageArray[0]
-            num2 = +messageArray[1]
+            const messageArray1 = displayOnScreen.textContent.split(operator)
+            num1 = +messageArray1[0]
 
-            calculatorResult = operate(num1, num2, operator)
+
+            const messageArray2 = messageArray1[1].split('=')
+            num2 = +messageArray2[0]
+
+            console.log(`num1: ${num1}`)
+            console.log(`num2: ${num2}`)
+           
+
+            calculatorResult = operate(num1, operator, num2)
+
+            // console.log(`calcularot result: ${calculatorResult}`)
 
             displayOnScreen.textContent = calculatorResult
 
